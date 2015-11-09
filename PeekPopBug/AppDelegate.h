0d9def423ba8d1abc2e92ba987329a9fc80abba0
Stephen Whitfield <stephen@cyberdust.com>
2015-11-09 10:12:26 -0800

    Initial Commit

---
+++ PeekPopBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeekPopBug
+//
+//  Created by Stephen Whitfield on 11/9/15.
+//  Copyright © 2015 Stephen Whitfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PeekPopBug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PeekPopBug
+//
+//  Created by Stephen Whitfield on 11/9/15.
+//  Copyright © 2015 Stephen Whitfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
